897697345985b3ab2abcb14668cb9b82f652a18d
Jonathan Pinlac <jonppinlac@gmail.com>
2018-02-12 16:48:43 -0500

    Add removeString

---
+++ sesh3/removeString.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+typedef enum { false, true } bool;
+
+void removeString(char string[]) {
+  
+}
+int main(int argc, char *argv[]) {
+  char firstString[] = "abcdef";
+  char secondString[] = "The bird";
+  printf("%s", firstString);
+  printf("%s", secondString);
+}
